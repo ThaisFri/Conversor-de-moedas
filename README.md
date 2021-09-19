ab9667a3fe31658b30fbceaed4c434b62e755ac1
Thais <thais.frisselli@hotmail.com>
2021-09-18 21:20:44 -0300

    fisrt commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Conteúdo detalhado desta aula:
+ -Interagir com usuário para receber os preços a serem convertidos;
+ -Criar a lógica para saber como converter;
+ -Utilizar funções que interagem com a tela;
+ -Exibir o resultado na tela com innerHTML;
+
+ extra: adicionado conversor de Bitcoin e de Euro
---
+++ script.js
@@ -0,0 +1,38 @@
+function ConverterDolar() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorReal = parseFloat(valor);
+
+  var valorDolar = valorReal * 5;
+  console.log(valorDolar);
+
+  var elementoValorConvertidoDolar = document.getElementById("valorConvertidoDolar");
+  var valorConvertidoDolar = "O valor em dolar é U$ " + valorDolar;
+  elementoValorConvertidoDolar.innerHTML = valorConvertidoDolar;
+}
+
+function ConverterEuro() {
+    var valorElemento = document.getElementById("valorEuro");
+    var valor = valorElemento.value;
+    var valorReal = parseFloat(valor);
+  
+    var valorEuro = valorReal * 6;
+    console.log(valorEuro);
+  
+    var elementoValorConvertidoEuro = document.getElementById("valorConvertidoEuro");
+    var valorConvertidoEuro = "O resultado em euro é € " + valorEuro;
+    elementoValorConvertidoEuro.innerHTML = valorConvertidoEuro;
+  }
+
+  function ConverterBitcoin() {
+    var valorElemento = document.getElementById("valorBitcoin");
+    var valor = valorElemento.value;
+    var valorReal = parseFloat(valor);
+  
+    var valorBitcoin = valorReal / 247.500;
+    console.log(valorBitcoin);
+  
+    var elementoValorConvertidoBitcoin = document.getElementById("valorConvertidoBitcoin");
+    var valorConvertidoBitcoin = "O resultado em Bitcoin é ฿ " + valorBitcoin;
+    elementoValorConvertidoBitcoin.innerHTML = valorConvertidoBitcoin;
+  }
